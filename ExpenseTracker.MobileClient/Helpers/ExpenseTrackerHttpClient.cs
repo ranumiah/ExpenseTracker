b55bafb928e9c5018dcc0132b532fd9fc717fbfb
Raiegh <ranumiah@gmail.com>
2016-09-14 11:49:17 +0100

    Added Windows 8.1 Mobile Project

---
+++ ExpenseTracker.MobileClient/Helpers/ExpenseTrackerHttpClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using ExpenseTracker.Constants;
+using Marvin.HttpCache;
+
+
+namespace ExpenseTracker.MobileClient.Helpers
+{
+
+    public static class ExpenseTrackerHttpClient
+    {
+
+        private static HttpClient _currentClient = null;
+
+        public static HttpClient GetClient()
+        {
+            if (_currentClient == null)
+            {
+
+                _currentClient = new HttpClient(new HttpCacheHandler {InnerHandler = new HttpClientHandler()});
+
+                _currentClient.BaseAddress = new Uri(ExpenseTrackerConstants.ExpenseTrackerApi);
+
+                _currentClient.DefaultRequestHeaders.Accept.Clear();
+                _currentClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+ 
+            }
+
+            return _currentClient;
+        }
+         
+    }
+
+     
+}
